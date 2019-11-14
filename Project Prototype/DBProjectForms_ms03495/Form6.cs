1eba44305e471e7aed7d16ef9cffba47901bbe57
MTalhaS123 <43110392+MTalhaS123@users.noreply.github.com>
2019-11-14 22:57:30 +0500

    Project Forms for Staff and related entities
    We have each uploaded the forms corresponding to the ERD we presented earlier. They are not linked to one another as of now due to time constrainsts because the login form will redirect the user according to the category he/she belongs to. This can only be done once we have the IDs and user names segregated according to student,faculty and staff.

---
+++ Project Prototype/DBProjectForms_ms03495/Form6.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form6 : Form
+    {
+        public Form6()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Faculty added successfully");
+        }
+    }
+}
